4ca36fa8547f3810b9791c1bcc8b7b91d85e60b4
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-07-11 20:06:39 -0300

    Config jest to report the test results in a format that is compatible with Qase

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,6 +3,18 @@
 const config = {
   rootDir: '../',
   preset: '@redwoodjs/testing/config/jest/api',
+  reporters: [
+    'default',
+    [
+      'jest-qase-reporter',
+      {
+        apiToken: process.env.QASE_API_TOKEN,
+        projectCode: process.env.QASE_PROJECT_CODE,
+        logging: true,
+        runComplete: true,
+      },
+    ],
+  ],
 }
 
 module.exports = config
