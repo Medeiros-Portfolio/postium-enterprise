9ef5769923a93ade78deb170ff13751a412a268a
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-07-16 18:45:13 -0300

    Fix: Sign up
    Squashed commit of the following:
    
    commit 90e19f0cff494fa853638ef2ca5de36aba92a472
    Author: Rafael Medeiros <rafaelmedrib@gmail.com>
    Date:   Sun Jul 16 18:43:30 2023 -0300
    
        Add validation to name field in sign up form
    
    commit 31d36016510da4fba71421f092f0a92e816b1a7e
    Author: Rafael Medeiros <rafaelmedrib@gmail.com>
    Date:   Sun Jul 16 18:42:21 2023 -0300
    
        Add name to sign up handler
    
    commit 78ad0dfa9cb333d4544e1780e313e09f5781db6d
    Author: Rafael Medeiros <rafaelmedrib@gmail.com>
    Date:   Sun Jul 16 18:36:20 2023 -0300
    
        Remove default name from user

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -110,10 +110,15 @@ export const handler = async (
     // `signUp()` function in the form of: `{ message: 'String here' }`.
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
       validate(username, 'email', { email: true })
+      validate(userAttributes.name, 'name', {
+        presence: true,
+        length: { minimum: 3 },
+      })
 
       return db.user.create({
         data: {
           email: username,
+          name: userAttributes.name,
           hashedPassword: hashedPassword,
           salt: salt,
           // name: userAttributes.name
