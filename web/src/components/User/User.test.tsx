1ed081df0a3fe99e1800ef43eea3b81e17f3e325
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-07-21 15:12:56 -0300

    Fix broken web tests
    Squashed commit of the following:
    
    commit 45d39b69f1f004c934d5a90e81bce837ffb72739
    Author: Rafael Medeiros <rafaelmedrib@gmail.com>
    Date:   Fri Jul 21 15:12:14 2023 -0300
    
        Fix broken web tests

--- web/src/components/User/User.test.tsx
+++ web/src/components/User/User.test.tsx
@@ -7,8 +7,25 @@ import User from './User'
 
 describe('User', () => {
   it('renders successfully', () => {
+    const user = {
+      id: '1',
+      name: 'John Doe',
+      email: 'john.doe@fakeemail.com',
+      avatar: 'https://cdn.filestackcontent.com/TMo43YTaQNK6UdY7SSen',
+      roles: ['admin'],
+    }
+
     expect(() => {
-      render(<User />)
+      render(
+        <User
+          avatar={user.avatar}
+          email={user.email}
+          id={user.id}
+          name={user.name}
+          roles={user.roles}
+          key={(10 * Math.random()).toFixed(0)}
+        />
+      )
     }).not.toThrow()
   })
 })
